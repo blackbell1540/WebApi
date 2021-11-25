d28616bef61d4d181f75b60e1409450cf668bbb2
jiyoon <shy817@naver.com>
2021-11-25 14:08:27 +0900

    index page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  children: [
+    {
+      path: '',
+      pathMatch: 'full',
+      redirectTo: 'index'
+    },
+    {
+      path: 'index',
+      loadChildren: () => import('./page/index/index.module').then(m => m.IndexModule)
+    },
+    {
+      path: 'background-task',
+      loadChildren: () => import('./page/background-task/background-task.module').then(m => m.BackgroundTaskModule)
+    },
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
